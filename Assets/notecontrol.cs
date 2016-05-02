ea5c80605a7352d3836383ff192a094c32dadc54
RJ Santos <rjs4nt0s@yahoo.com>
2016-05-01 23:48:10 -0400

    V0.0.1

---
+++ Assets/notecontrol.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class notecontrol : MonoBehaviour {
+
+    public Transform burst;
+
+	// Use this for initialization
+
+	void Start () {
+	    if (gameObject.name == "Note1(Clone)")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3.75f, 0);
+        }
+        if (gameObject.name == "Note2(Clone)")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3.75f, 0);
+        }
+        if (gameObject.name == "Note3(Clone)")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3.75f, 0);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if ((songcontrol.destroyLeft == "y") && (gameObject.name == "Note1(Clone)"))
+        {
+            Destroy(gameObject);
+            Instantiate(burst,transform.position, burst.rotation);
+        }
+        if ((songcontrol.destroyDown == "y") && (gameObject.name == "Note2(Clone)"))
+        {
+            Destroy(gameObject);
+            Instantiate(burst, transform.position, burst.rotation);
+        }
+        if ((songcontrol.destroyRight == "y") && (gameObject.name == "Note3(Clone)"))
+        {
+            Destroy(gameObject);
+            Instantiate(burst, transform.position, burst.rotation);
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(0, 255, 0);
+    }
+    void OnTriggerExit2D()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/songcontrol.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class songcontrol : MonoBehaviour {
+
+    public Transform Note1;
+    public Transform Note2;
+    public Transform Note3;
+
+    public float songlength = 0;
+
+    public KeyCode keyLeft;
+    public KeyCode keyDown;
+    public KeyCode keyRight;
+
+    public static string destroyLeft = "n";
+    public static string destroyDown = "n";
+    public static string destroyRight = "n";
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        songlength += Time.deltaTime;
+
+        if((songlength >= .1) && (songlength <= .125))
+        {
+            Instantiate(Note1, Note1.position, Note1.rotation);
+        }
+        if ((songlength >= 1) && (songlength <= 1.025))
+        {
+            Instantiate(Note2, Note2.position, Note2.rotation);
+        }
+        if ((songlength >= 1.5) && (songlength <= 1.525))
+        {
+            Instantiate(Note3, Note3.position, Note3.rotation);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(keyLeft) && (other.gameObject.name == "Note1(Clone)"))
+        {
+            destroyLeft = "y";
+        }
+        if (Input.GetKeyDown(keyDown) && (other.gameObject.name == "Note2(Clone)"))
+        {
+            destroyDown = "y";
+        }
+        if (Input.GetKeyDown(keyRight) && (other.gameObject.name == "Note3(Clone)"))
+        {
+            destroyRight = "y";
+        }
+    }
+}
